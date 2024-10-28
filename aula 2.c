6a1a501e1ed826e596744c416cb14de5967fa4b9
matheus1902-code <matheusvm2019@gmail.com>
2024-10-28 08:54:23 -0300

    Add files via upload

---
+++ aula 2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int numGlonal;
+
+int main(){
+	
+	float numQuebrado;
+	
+	char letter = 'a';
+	bool ativo = false;
+	int i;
+
+	for(i = 0; i<= 10; i++){
+		printf("%d\n",i);
+	
+	}
+	
+	while(!ativo){
+	   printf("Olá, voce esta ativo");     
+	   ativo = true;
+	}
+};
